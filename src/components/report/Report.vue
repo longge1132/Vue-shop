bef413a25f892f61c68dd9636d85d0db27892193
hailong <1132555965@qq.com>
2021-11-30 21:22:57 +0800

    完成数据报表

---
+++ src/components/report/Report.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <!--    头部导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <!-- 为 ECharts 准备一个定义了宽高的 DOM -->
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>// 1.导入echarts
+// import echarts from 'echarts'
+import * as echarts from 'echarts'
+
+export default {
+  name: 'Report',
+  data () {
+    return {
+      // 准备数据所需要的配置项
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  async mounted () {
+    // 基于准备好的dom，初始化echarts实例, mounted 时 页面 元素渲染完毕可以操作 dom
+    const myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) return this.$message.error('获取数据失败')
+    // const option = {
+    //   title: {
+    //     text: 'ECharts 入门示例'
+    //   },
+    //   tooltip: {},
+    //   legend: {
+    //     data: ['销量']
+    //   },
+    //   xAxis: {
+    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    //   },
+    //   yAxis: {},
+    //   series: [
+    //     {
+    //       name: '销量',
+    //       type: 'bar',
+    //       data: [5, 20, 36, 10, 10, 20]
+    //     }
+    //   ]
+    // }
+    // 使用刚指定的配置项和数据显示图表。
+    const result = { ...this.options, ...res.data }
+    myChart.setOption(result)
+  }
+}
+</script>
